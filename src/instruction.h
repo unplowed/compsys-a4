7fc3a0830e0c35db4a766ee97168769117b246e7
Snorre Ettrup Altschul <snorre@altschul.dk>
2025-11-26 05:21:57 +0100

    Implemented more instructions

--- src/instruction.h
+++ src/instruction.h
@@ -1,6 +1,22 @@
 typedef enum opcode {
-  OP_AUIPC = 0b0010111, // Add upper immediate to PC
-  OP_ADDI =  0b0010011, // ADDI adds the sign-extended 12-bit immediate to register rs1
+  // Add upper immediate to PC
+  OP_AUIPC = 0b0010111,
+  // ADDI adds the sign-extended 12-bit immediate to register rs1
+  OP_ADDI = 0b0010011,
+  // Load Upper Immediate
+  OP_LUI = 0b0110111,
+  // jump and link
+  OP_JAL = 0b1101111,
+  // loads a 32-bit value from memory into rd
+  OP_LW = 0b0000011,
+  // stores a 32-bit value from the register rs2 to memory.
+  OP_SW = 0b0100011,
+  // indirect jump instruction JALR (jump and link register)
+  OP_JALR = 0b1100111,
+  // jumps if rs1 == rs2, otherwise goes to next instruction
+  OP_BEQ = 0b1100011,
+  // service call to execution environment
+  OP_ECALL = 0b1110011,
 } opcode_t;
 
 typedef union __attribute__((packed)) instruction_args {
@@ -35,10 +51,8 @@ typedef union __attribute__((packed)) instruction_args {
   struct __attribute__((packed)) j_type {
     opcode_t opcode : 7;
     unsigned int rd : 5;
-    unsigned int imm19 : 8;
-    unsigned int imm20 : 1;
-    unsigned int imm30 : 10;
-    unsigned int imm31 : 1;
+    int imm11 : 9;
+    int imm20 : 11;
   } J;
 } instruction_args_t;
 
--- src/simulate.c
+++ src/simulate.c
@@ -43,7 +43,22 @@ int write_register(registers_t *registers, int register_idx, int value) {
 }
 
 int read_register(const registers_t *registers, int register_idx, int *out) {
+  if (debug) {
+    printf("reading register %i (%s): ", register_idx,
+           register_names[register_idx]);
+
+    if (register_idx < 0 || register_idx > 31) {
+      printf("Out of bounds read!\n");
+      assert(0);
+    }
+  }
+
   *out = registers->unnamed[register_idx];
+
+  if (debug) {
+    printf("%i\n", *out);
+  }
+
   return 0;
 }
 
@@ -51,21 +66,20 @@ struct Stat simulate(struct memory *mem, int start_addr, FILE *log_file,
                      struct symbols *symbols) {
   int run = 1;
   registers_t registers;
+  memset(&registers, 0, sizeof(registers_t));
   registers.named.pc = start_addr;
 
   while (run) {
     int m = memory_read_word(mem, registers.named.pc);
     instruction_t *op = (instruction_t *)&m;
 
-    for (int i = 0; i < sizeof(instruction_t); i++) {
-      printf(BYTE_TO_BINARY_PATTERN, BYTE_TO_BINARY((long)op + i));
-      printf(" ");
-    }
-    printf("\n");
+    if (debug)
+      printf("pc: %8x\n", registers.named.pc);
 
     switch (op->opcode) {
+
     case OP_AUIPC:
-      write_register(&registers, op->args.U.rd >> 1,
+      write_register(&registers, op->args.U.rd,
                      registers.named.pc + (op->args.U.imm20 << 12));
       break;
     case OP_ADDI: {
@@ -74,6 +88,65 @@ struct Stat simulate(struct memory *mem, int start_addr, FILE *log_file,
       write_register(&registers, op->args.I.rd, rs1 + op->args.I.imm12);
       break;
     }
+    case OP_LUI:
+      write_register(&registers, op->args.U.rd, op->args.U.imm20 << 12);
+      break;
+
+    case OP_JAL:
+      write_register(&registers, op->args.J.rd, registers.named.pc + 4);
+      registers.named.pc += (op->args.J.imm20 << 1);
+      printf("jumping to %8x (%i)\n", registers.named.pc + 4,
+             (op->args.J.imm20 << 1));
+      break;
+
+    case OP_JALR: {
+      int addr_start;
+      read_register(&registers, op->args.I.rs1, &addr_start);
+      int addr =
+          (addr_start + op->args.I.imm12) & 0b11111111111111111111111111111110;
+      write_register(&registers, op->args.J.rd, registers.named.pc + 4);
+      registers.named.pc = addr;
+      printf("jumping to %8x\n", addr);
+      break;
+    }
+
+    case OP_BEQ: {
+      int rs1;
+      read_register(&registers, op->args.S.rs1, &rs1);
+      int rs2;
+      read_register(&registers, op->args.S.rs2, &rs2);
+      if (rs1 == rs2) {
+        registers.named.pc += (op->args.S.imm31 << 1);
+        printf("branching to %8x (%i)\n", registers.named.pc + 4,
+               (op->args.S.imm31 << 1));
+      }
+      break;
+    }
+
+    case OP_LW: {
+      int addr_start;
+      read_register(&registers, op->args.I.rs1, &addr_start);
+      int memory_addr = addr_start + op->args.I.imm12;
+      int word = memory_read_word(mem, memory_addr);
+      write_register(&registers, op->args.I.rd, word);
+      break;
+    }
+
+    case OP_SW: {
+      int addr_start;
+      read_register(&registers, op->args.S.rs1, &addr_start);
+      int memory_addr = addr_start + op->args.S.imm11;
+      int value;
+      read_register(&registers, op->args.S.rs2, &value);
+      memory_write_word(mem, memory_addr, value);
+      break;
+    }
+
+    case OP_ECALL: {
+      // yah mate idfk
+      // TODO: Implement syscalls
+    }
+
     default:
       unknown_instruction(op);
       assert(0);
